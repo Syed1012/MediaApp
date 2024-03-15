1286e0c1014993db2afa25a4c56beaf1157d74b6
Syed1012 <syedmusaib66@gmail.com>
2024-03-15 10:48:03 +0530

    small change

--- server/middleware/verifyToken.js
+++ server/middleware/verifyToken.js
@@ -1,19 +1,19 @@
-import JWT from 'jsonwebtoken';
+import JWT from "jsonwebtoken";
 
 const verifyToken = (req, res, next) => {
-    const token = req.headers.authorization;
+  const token = req.headers.authorization;
 
-    if (!token) {
-        return res.status(403).json({ message: 'Token is required' });
-    }
+  if (!token) {
+    return res.status(403).json({ message: "Token is required" });
+  }
 
-    try {
-        const decoded = JWT.verify(token, process.env.JWT_SECRET);
-        req.user = decoded;
-        next();
-    } catch (err) {
-        return res.status(401).json({ message: 'Invalid token' });
-    }
+  try {
+    const decoded = JWT.verify(token, process.env.JWT_SECRET);
+    req.user = decoded;
+    next();
+  } catch (err) {
+    return res.status(401).json({ message: "Invalid token" });
+  }
 };
 
 export default verifyToken;
